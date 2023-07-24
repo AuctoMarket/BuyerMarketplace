796c0d512d4f326db314c9641539182a98f5025e
Phat Pham <phat@onroads.xyz>
2023-07-24 10:36:13 +0700

    feat(AUcTO-22): add CSS Module & Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.scss';
+import HoldingPage from './pages/holding';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HoldingPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
