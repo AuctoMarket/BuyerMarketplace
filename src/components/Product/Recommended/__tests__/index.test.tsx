8c406662f2143ed51c4225135bd5aa70f1f0b837
Phat Pham <phat@onroads.xyz>
2023-07-25 14:22:08 +0700

    feat(AUCTO-20): create Recommended for you component

---
+++ src/components/Product/Recommended/__tests__/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Recommended from '..';
+
+test('renders Recommended', () => {
+  render(
+    <Recommended
+      data={{
+        products: [
+          {
+            image: 'test',
+            title: 'test',
+            sellerInfo: {
+              name: 'test',
+              avatar: 'test',
+              isVerified: true,
+              numReviews: 1,
+            },
+            purchase: {
+              currentBid: 1,
+              numBids: 1,
+              buyNowPrice: 1,
+            },
+            postedDate: 'test',
+          },
+        ],
+      }}
+      onShowMore={() => {}}
+      role="test"
+    />,
+  );
+  const text = screen.getByRole('test');
+  expect(text).toBeInTheDocument();
+});
---
+++ src/components/Product/Recommended/index.tsx
@@ -0,0 +1,68 @@
+import React, { ComponentProps } from 'react';
+
+import styles from './index.module.scss';
+import List from '../../List';
+import Card from '../Card';
+import Button from '../../Button';
+
+interface Props extends ComponentProps<'div'> {
+  data: {
+    products: {
+      image: string;
+      title: string;
+      sellerInfo: {
+        name: string;
+        avatar: string;
+        isVerified: boolean;
+        numReviews: number;
+      };
+      purchase: {
+        currentBid: number;
+        numBids: number;
+        buyNowPrice: number;
+      };
+      postedDate: string;
+    }[];
+  };
+  onShowMore: () => void;
+  showMoreText?: string;
+}
+
+export function ProductRecommended({
+  className,
+  data,
+  onShowMore,
+  showMoreText = 'Show more',
+  ...rest
+}: Props) {
+  const handleShowMore = () => {
+    onShowMore();
+  };
+
+  return (
+    <div className={`${styles['container']} ${className}`} {...rest}>
+      <div className={styles['heading']}>Recommended for you</div>
+
+      <div className={styles['list-container']}>
+        <List
+          className={styles['list']}
+          items={data.products.map((item, index) => (
+            <Card data={item} key={index} />
+          ))}
+        />
+      </div>
+
+      <div className={styles['show-more-container']}>
+        <Button
+          className={styles['show-more']}
+          theme="black"
+          onClick={handleShowMore}
+        >
+          {showMoreText}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductRecommended;
