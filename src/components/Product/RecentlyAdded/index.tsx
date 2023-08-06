e6143b99c6c5ec53980ddf6054f6f5f57c6e1eb4
Phat Pham <phatttpham9@gmail.com>
2023-08-06 20:41:13 +0700

    feat(AUCTO-47): create RecentlyAdded component

---
+++ src/components/Product/RecentlyAdded/index.tsx
@@ -0,0 +1,48 @@
+import React, { ComponentProps } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './index.module.scss';
+import List from '../../List';
+import Card from '../Card';
+import Icon from '../../Icon';
+
+import type { Product } from '../../../types/product.type';
+import type { Seller } from '../../../types/seller.type';
+
+interface Props extends ComponentProps<'div'> {
+  data: {
+    products: Product[];
+    seller: Seller;
+  };
+}
+
+export function ProductRecentlyAdded({
+  className,
+  data: { products, seller },
+  ...rest
+}: Props) {
+  return (
+    <div className={`${styles['container']} ${className}`} {...rest}>
+      <div className={styles['heading']}>
+        Recently Added
+        <Link to={`/sellers/${seller.id}/products`}>
+          See all deals
+          <Icon name="arrow-right" />
+        </Link>
+      </div>
+
+      <div className={styles['list-container']}>
+        <List
+          className={styles['list']}
+          items={products.map((product, index) => (
+            <Link to={`/products/${product.id}`}>
+              <Card data={{ product, seller }} key={index} />
+            </Link>
+          ))}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProductRecentlyAdded;
