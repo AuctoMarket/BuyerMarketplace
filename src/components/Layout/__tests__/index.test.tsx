8c899aa8828807824e8256e7c0cb48d379820c37
Phat Pham <phatttpham9@gmail.com>
2023-08-14 01:23:34 +0700

    chore: update unit tests

--- src/components/Layout/__tests__/index.test.tsx
+++ src/components/Layout/__tests__/index.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Layout from '..';
 
 test('renders Layout', () => {
-  render(<Layout>test</Layout>);
+  render(
+    <BrowserRouter>
+      <Layout>test</Layout>
+    </BrowserRouter>,
+  );
   const text = screen.getByText('test');
   expect(text).toBeInTheDocument();
 });
