12ac29429e03c75c4b886276404c54f636d9add6
Phat Pham <140833423+phatttpham9@users.noreply.github.com>
2023-08-14 10:33:56 +0700

    feat(AUCTO-69): update Header NavBar (2nd launch) (#41)

--- src/components/Header/__tests__/index.test.tsx
+++ src/components/Header/__tests__/index.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Header from '..';
 
 test('renders Header', () => {
-  render(<Header />);
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>,
+  );
   const text = screen.getByAltText('logo-horizontal-inverted-color');
   expect(text).toBeInTheDocument();
 });
