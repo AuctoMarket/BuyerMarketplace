1fc33795772c1b7f63a5862faa1c352fd9ea3beb
Phat Pham <phatttpham9@gmail.com>
2023-08-14 19:00:48 +0700

    feat(AUCTO-57): update useSeller hook

--- src/hooks/useSeller.ts
+++ src/hooks/useSeller.ts
@@ -12,9 +12,11 @@ const useSeller = (id: string) => {
     data: seller,
     error,
   } = useSWR(`/sellers/${id}`, async (url: string) => {
+    if (!id) return null;
+
     const response = await axios.get<Seller>(`${apiConfig.baseUrl}${url}`);
 
-    return transformSeller(response.data);
+    return { ...transformSeller(response.data), isVerified: true };
   });
 
   return {
