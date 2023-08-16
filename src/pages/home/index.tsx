9387844b4d50527c91d664b64ebe56212abb0228
Phat Pham <phatttpham9@gmail.com>
2023-08-17 00:35:02 +0700

    feat(AUCTO-72): revert home/index.tsx and holding/index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,12 +6,44 @@ import ProductPromotion from '../../components/Product/Promotion';
 import ProductRecentlyAdded from '../../components/Product/RecentlyAdded';
 import SellerPromotion from '../../components/Sellers/Promotion';
 import useProductsList from '../../hooks/useProductsList';
-import Header from '../../components/Checkout/CheckoutDetails/Header';
 
 function HomePage() {
   const { productsList: recentlyAdded = [] } = useProductsList();
 
-  return <Header data={{ number: '1', text: 'Contact Details' }} />;
+  return (
+    <Layout>
+      <div className={styles['home-page']}>
+        <ProductPromotion
+          className={styles['promotion']}
+          data={{
+            images: [
+              '/images/promotion/product-banner.png',
+              '/images/promotion/product-banner.png',
+              '/images/promotion/product-banner.png',
+            ],
+            url: '#',
+            title:
+              '<p>Collection: Pokemon S&S Brilliant</p><p>StarsPre-Order yours today!</p>',
+          }}
+        />
+
+        {recentlyAdded.length > 0 && (
+          <div className={styles['content']}>
+            <ProductRecentlyAdded
+              data={{
+                products: recentlyAdded,
+                seeMore: false,
+              }}
+            />
+          </div>
+        )}
+
+        <div className={styles['seller-promotion']}>
+          <SellerPromotion />
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default HomePage;
