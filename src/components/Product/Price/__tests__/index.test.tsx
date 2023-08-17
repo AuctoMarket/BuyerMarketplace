1949ad930a6b68d643efbede9091e4e81923f5fc
Phat Pham <phatttpham9@gmail.com>
2023-08-17 18:11:15 +0700

    chore: format Price has 2 decimals

--- src/components/Product/Price/__tests__/index.test.tsx
+++ src/components/Product/Price/__tests__/index.test.tsx
@@ -6,10 +6,16 @@ import Price from '..';
 import type { Product } from '../../../../types/product.type';
 
 describe('Price', () => {
-  const data: Pick<Product, 'price'> = { price: 1 };
+  const data: Pick<Product, 'price'> = { price: 10 };
 
   test('renders Price', () => {
     render(<Price data={data} role="test" />);
+    const text = screen.getByRole('test');
+    expect(text).toBeInTheDocument();
+  });
+
+  test('renders Price with decimal', () => {
+    render(<Price data={{ price: 11 }} role="test" />);
     const text = screen.getByRole('test');
     expect(text).toBeInTheDocument();
   });
--- src/components/Product/Price/index.tsx
+++ src/components/Product/Price/index.tsx
@@ -9,9 +9,20 @@ interface Props extends React.ComponentProps<'div'> {
 }
 
 function ProductPrice({ className, data: { price }, ...rest }: Props) {
+  const priceInDollars = price / 10;
+
   return (
     <div className={`${styles['product-price']} ${className}`} {...rest}>
-      ${new Intl.NumberFormat('en-SG').format(price / 10)}
+      $
+      {new Intl.NumberFormat(
+        'en-SG',
+        priceInDollars % 1 !== 0
+          ? {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            }
+          : {},
+      ).format(priceInDollars)}
     </div>
   );
 }
