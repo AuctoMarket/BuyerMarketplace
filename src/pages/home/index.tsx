04eaf847a30c1f808f2ba43e64fd2ecd618e9b87
Phat Pham <phatttpham9@gmail.com>
2023-08-19 14:06:56 +0700

    chore: revert home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,6 @@ import ProductPromotion from '../../components/Product/Promotion';
 import ProductRecentlyAdded from '../../components/Product/RecentlyAdded';
 import SellerPromotion from '../../components/Sellers/Promotion';
 import useProductsList from '../../hooks/useProductsList';
-import ContactDetails from '../../components/Checkout/ContactDetails';
 
 function HomePage() {
   const { productsList: recentlyAdded = [] } = useProductsList();
@@ -26,13 +25,6 @@ function HomePage() {
             title:
               '<p>Collection: Pokemon S&S Brilliant</p><p>StarsPre-Order yours today!</p>',
           }}
-        />
-
-        <ContactDetails
-          onEmailChange={() => {}}
-          onPhoneNumberChange={() => {}}
-          onEmailConfirmChange={() => {}}
-          onTelegramHandleChange={() => {}}
         />
 
         {recentlyAdded.length > 0 && (
