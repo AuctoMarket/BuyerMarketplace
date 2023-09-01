9a642a7087d81d1fc7144251ca00a13def1bd9c8
Phat Pham <phatttpham9@gmail.com>
2023-09-01 17:26:55 +0700

    feat(AUCTO): change login & sign button styles; add Contact Us button on desktop & mobile

--- src/components/Header/__tests__/index.test.tsx
+++ src/components/Header/__tests__/index.test.tsx
@@ -14,29 +14,3 @@ test('renders Header', () => {
   const text = screen.getByAltText('logo-horizontal-inverted-color');
   expect(text).toBeInTheDocument();
 });
-
-// Test if login and signup buttons are rendered when the user is not logged in
-test('renders login and signup buttons when user is not logged in', () => {
-  render(
-    <BrowserRouter>
-      <Header />
-    </BrowserRouter>,
-  );
-  const loginButton = screen.getByTestId('login-desktop-button');
-  const signupButton = screen.getByTestId('signup-desktop-button');
-  expect(loginButton).toBeInTheDocument();
-  expect(signupButton).toBeInTheDocument();
-});
-
-// Test if mobile login and signup buttons are rendered when the user is not logged in
-test('renders mobile login and signup buttons when user is not logged in', () => {
-  render(
-    <BrowserRouter>
-      <Header />
-    </BrowserRouter>,
-  );
-  const mobileLoginButton = screen.getByTestId('login-mobile-button');
-  const mobileSignupButton = screen.getByTestId('signup-mobile-button');
-  expect(mobileLoginButton).toBeInTheDocument();
-  expect(mobileSignupButton).toBeInTheDocument();
-});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,6 @@ import styles from './index.module.scss';
 import Logo from '../Logo';
 import List from '../List';
 import Icon from '../Icon';
-import Dropdown from '../Dropdown';
 import LoginForm from '../../components/LoginForm';
 import SignupForm from '../../components/SignupForm';
 import useAuth from '../../hooks/useAuth';
@@ -16,27 +15,6 @@ interface Props extends ComponentProps<'div'> {}
 function Header({ className, ...rest }: Props) {
   const { togglePopup } = useContext(PopupContext);
   const { user, login, signup } = useAuth();
-
-  const openLoginForm = () => {
-    if (togglePopup) {
-      togglePopup(true, <LoginForm onLogin={handleLogin} />);
-    }
-  };
-
-  const openSignupForm = () => {
-    if (togglePopup) {
-      togglePopup(true, <SignupForm onSignup={handleSignup} />);
-    }
-  };
-
-  const handleLogin = async (email: string, password: string) => {
-    await login(email, password);
-
-    // Close the login popup after successful login
-    if (togglePopup) {
-      togglePopup(false);
-    }
-  };
 
   const handleSignup = async (email: string, password: string) => {
     await signup(email, password);
@@ -52,6 +30,49 @@ function Header({ className, ...rest }: Props) {
       <Link className={styles['logo']} to="/">
         <Logo type="horizontal" theme="inverted-color" />
       </Link>
+
+      <List
+        className={styles['navbar-right']}
+        items={[
+          ...(!user
+            ? [
+                <Link className={styles['browse']} to="/">
+                  Browse
+                </Link>,
+                <Link className={styles['about-us']} to="#">
+                  About Us
+                </Link>,
+                <Link
+                  className={styles['contact-us']}
+                  to="https://t.me/auctomarketplace"
+                  target="_blank"
+                >
+                  Contact Us
+                </Link>,
+                <Link className={styles['sign-in']} to="/auth/login">
+                  Sign In
+                </Link>,
+              ]
+            : [
+                <Link className={styles['browse']} to="/">
+                  Browse
+                </Link>,
+                <Link className={styles['about-us']} to="#" target="_blank">
+                  About Us
+                </Link>,
+                <Link
+                  className={styles['contact-us']}
+                  to="https://t.me/auctomarketplace"
+                  target="_blank"
+                >
+                  Contact Us
+                </Link>,
+                <Link to="#" target="_blank">
+                  <Icon className={styles['icon-user']} name="user" />
+                </Link>,
+              ]),
+        ]}
+      />
     </div>
   );
 }
