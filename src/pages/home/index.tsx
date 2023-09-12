495369cf4b0878119f8f097c80e93833faec6b52
Phat Pham <phatttpham9@gmail.com>
2023-09-12 23:45:28 +0700

    chore: update styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 
 import styles from './index.module.scss';
 import Layout from '../../components/Layout';
-import ProductPromotion from '../../components/Product/Promotion';
 import ProductPreOrder from '../../components/Product/PreOrder';
 import ProductRecentlyAdded from '../../components/Product/RecentlyAdded';
-import SellerPromotion from '../../components/Sellers/Promotion';
 import useProductsList from '../../hooks/useProductsList';
 import { ProductType } from '../../types/product.type';
 import Banner from '../../components/Banner';
-
-function isMobile() {
-  return window.innerWidth <= 820;
-}
 
 function HomePage() {
   const { productsList: preOrder = [] } = useProductsList({
@@ -26,31 +20,26 @@ function HomePage() {
   return (
     <Layout>
       <div className={styles['home-page']}>
-        <div className={styles['content']}>
-          <Banner className={styles['banner']} />
-          {preOrder.length > 0 && (
-            <ProductPreOrder
-              className={styles['pre-order']}
-              data={{
-                products: preOrder,
-              }}
-            />
-          )}
+        <Banner className={styles['banner']} />
 
-          {recentlyAdded.length > 0 && (
-            <ProductRecentlyAdded
-              className={styles['recently-added']}
-              data={{
-                products: recentlyAdded,
-                seeMore: false,
-              }}
-            />
-          )}
-        </div>
+        {preOrder.length > 0 && (
+          <ProductPreOrder
+            className={styles['pre-order']}
+            data={{
+              products: preOrder,
+            }}
+          />
+        )}
 
-        <div className={styles['seller-promotion']}>
-          <SellerPromotion />
-        </div>
+        {recentlyAdded.length > 0 && (
+          <ProductRecentlyAdded
+            className={styles['recently-added']}
+            data={{
+              products: recentlyAdded,
+              seeMore: false,
+            }}
+          />
+        )}
       </div>
     </Layout>
   );
