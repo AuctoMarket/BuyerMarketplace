03bd45bf4921452477282e216bea2166cdf6fd13
Phat Pham <phatttpham9@gmail.com>
2023-09-19 02:55:27 +0700

    chore: update about us page

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -4,73 +4,57 @@ import styles from './index.module.scss';
 import Layout from '../../components/Layout';
 import ButtonLink from '../../components/Button/Link';
 import Image from '../../components/Image';
-
-function isMobile() {
-  return window.innerWidth <= 820;
-}
+import responsive from '../../utils/responsive';
 
 const AboutUsPage = () => {
   return (
-    <Layout className={styles['about-us-container']}>
+    <Layout>
       <div className={styles['about-us-page']}>
-        <div className={`${styles.top} ${isMobile() ? styles.gradient : ''}`}>
-          <div className={styles['left']}>
+        <div className={styles.top}>
+          <div className={styles.left}>
             About Us
             <div className={styles['content-1']}>
               <span>Making collecting accessible</span>, lorem ipsum dolor sit
-              ametous, consectetur adipiscing elit.
+              ametous, consectetur adipiscing elit. Maecenas et leo non massa
+              egestas tristique, congue tellus mattis ultrices.
             </div>
-            <div className={styles['conntent-2']}>
+            <div className={styles['content-2']}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, suradora
               do eiusmod tempor incididunt ut labore et dolore. Elsem etudar
               elit marcenas et leon non massue triteck attis ametous beotes.
             </div>
-            <ButtonLink to={`/`} className={styles['button']}>
+            <ButtonLink to={`/`} className={styles['button']} theme="color">
               Marketplace
             </ButtonLink>
           </div>
 
-          {isMobile() ? (
-            <>
-              <div className={styles['right-mobile']}>
-                <Image src="images/category/pokemon-cards-1-mobile.png" />
-              </div>
-            </>
-          ) : (
-            <div className={styles['right']}>
-              <Image src="images/category/pokemon-cards-1.png" />
-            </div>
-          )}
+          <div className={styles['right']}>
+            <Image src="images/about-us/pokemon-cards.png" />
+          </div>
+
+          {responsive.isMd() && <div className={styles['gradient']} />}
         </div>
 
         <div className={styles['bottom']}>
-          {isMobile() ? (
-            <div className={styles['left-mobile']}>
-              <Image src="images/category/pokemon-cards-2-mobile.png" />
-            </div>
-          ) : (
-            <div className={styles['left']}>
-              <Image src="images/category/pokemon-cards-2.png" />
-            </div>
-          )}
+          <div className={styles['left']}>
+            <Image src="images/about-us/pokemon-1.png" />
+          </div>
 
           <div className={styles['right']}>
             <div className={styles['content-1']}>
               <span>Making collecting safe</span>, lorem ipsum dolor sit
-              ametous, consectetur adipiscing elit. Maecenas et leo non massa.
+              ametous, consectetur adipiscing elit. Maecenas et leo non massa
+              egestas tristique, congue tellus mattis ultrices.
             </div>
 
             <div className={styles['content-2']}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               suradorased do eiusmod tempor incididunt ut labore et dolore.
               Elsem etudar elit marcenas et leon non massue triteck attis
-              ametous beotes. Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, suradorased do eiusmod tempor incididunt ut labore et
-              dolore. Elsem etudar elit marcenas et leon non massue triteck
-              attis ametous beotes.
+              ametous beotes.
             </div>
 
-            <ButtonLink to={`/`} className={styles['button']}>
+            <ButtonLink to={`/`} className={styles['button']} theme="color">
               Our Policies
             </ButtonLink>
           </div>
