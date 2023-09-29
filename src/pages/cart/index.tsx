af69e6cd22b84029a700270647ef189e86bb6a00
Phat Pham <phatttpham9@gmail.com>
2023-09-29 15:44:47 +0700

    feat(AUCTO-121): update  Cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../../components/Layout';
 import styles from './index.module.scss';
 import useCart from '../../hooks/useCart';
 import Image from '../../components/Image';
+import NumberInput from '../../components/NumberInput';
 import useProductByIds from '../../hooks/useProductByIds';
+import Button from '../../components/Button';
+import { Product } from '../../types/product.type';
 
-function CartPage() {
-  const { cartItems } = useCart();
+function CartPage({}) {
+  const { cartItems, updateCartItem } = useCart();
   const { products } = useProductByIds(
     cartItems.map(({ productId }) => productId),
   );
+
+  const onChangeQuantity = (quantity: number) => {};
+
+  const getQuantity = (product: Product) => {
+    return cartItems.find((item) => item.productId === product.id)
+      ?.quantity as number;
+  };
 
   return (
     <Layout>
@@ -25,14 +35,36 @@ function CartPage() {
                 </div>
                 <div className={styles['info']}>
                   <div className={styles['name']}>{product.title}</div>
-                  <div className={styles['quantity']}>{product.quantity}</div>
+                  <div className={styles['quantity-container']}>
+                    <label className={styles['label']}>Quantity:</label>
+                    <div className={styles['quantity']}>
+                      <NumberInput
+                        className={styles['quantity-input']}
+                        value={getQuantity(product)}
+                        onChangeValue={onChangeQuantity}
+                        min={1}
+                        max={product.quantity - product.soldQuantity}
+                      />
+                    </div>
+                  </div>
                   <div className={styles['price-remove']}>
-                    {product.price}
-                    Remove Item
+                    <div className={styles['price']}>
+                      ${product.price * getQuantity(product)}
+                    </div>
+                    <Button className={styles['remove']}>Remove Item</Button>
                   </div>
                 </div>
               </div>
             ))}
+        </div>
+        <div className={styles['footer']}>
+          <div className={styles['subtotal']}>
+            Subtotal: <span>$1231123</span>
+          </div>
+          <Button className={styles['checkout']}>Proceed to Checkout</Button>
+          <Button className={styles['chat']} theme="black">
+            Chat with us
+          </Button>
         </div>
       </div>
     </Layout>
