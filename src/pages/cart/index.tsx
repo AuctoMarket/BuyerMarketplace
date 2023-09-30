77c14cdca9eb3ed623f7ef64eeac4583ee0a10ff
Phat Pham <phatttpham9@gmail.com>
2023-09-30 16:28:29 +0700

    chore: handle popup

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,43 +1,96 @@
-import React from 'react';
+import React, { ComponentProps, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import Layout from '../../components/Layout';
 import styles from './index.module.scss';
+import Layout from '../../components/Layout';
 import useCart from '../../hooks/useCart';
 import Image from '../../components/Image';
 import NumberInput from '../../components/NumberInput';
-import useProductByIds from '../../hooks/useProductByIds';
 import Button from '../../components/Button';
 import ButtonLink from '../../components/Button/Link';
-import { useNavigate } from 'react-router-dom';
-import { Product } from '../../types/product.type';
+import Price from '../../components/Product/Price';
+import { PopupContext } from '../../components/Popup';
+import { Product, ProductType } from '../../types/product.type';
 import useAuth from '../../hooks/useAuth';
+import useProductByIds from '../../hooks/useProductByIds';
+
+interface PopupProps extends ComponentProps<'div'> {
+  data: {
+    product: Product;
+  };
+  onYes: () => void;
+  onNo: () => void;
+}
+
+function Popup({ data: { product }, onYes, onNo, ...rest }: PopupProps) {
+  return (
+    <div className={styles['popup']} {...rest}>
+      <div className={styles['popup-heading']}>
+        Are you sure you want to remove this item from your cart:
+      </div>
+      <div className={styles['popup-content']}>{product.title}</div>
+      <div className={styles['popup-footer']}>
+        <Button
+          className={styles['popup-yes-button']}
+          onClick={onYes}
+          theme="black"
+        >
+          Yes
+        </Button>
+        <Button
+          className={styles['popup-no-button']}
+          onClick={onNo}
+          theme="white"
+        >
+          No
+        </Button>
+      </div>
+    </div>
+  );
+}
 
 function CartPage() {
-  const { cartItems, updateCartItem, removeCartItem } = useCart();
-  const { products } = useProductByIds(
-    cartItems.map(({ productId }) => productId),
-  );
   const navigate = useNavigate();
   const { user } = useAuth();
-  const onChangeQuantity = (id: string, quantity: number) => {
-    const index = cartItems.findIndex((item) => item.productId === id);
-    if (index !== -1) {
+  const { cartItems, updateCartItem, removeCartItem } = useCart();
+  const { products = [] } = useProductByIds(
+    cartItems.map(({ productId }) => productId),
+  );
+  const { togglePopup } = useContext(PopupContext);
+
+  const handleChangeQuantity = (index: number, quantity: number) => {
+    if (quantity === 0) {
+      handleRemoveItem(index);
+      return;
+    } else {
       updateCartItem(index, quantity);
     }
   };
 
-  const getQuantity = (product: Product) => {
-    return cartItems.find((item) => item.productId === product.id)
-      ?.quantity as number;
-  };
+  const handleRemoveItem = (index: number) => {
+    if (!togglePopup) {
+      return;
+    }
 
-  const handleRemoveItem = (id: string) => {
-    const index = cartItems.findIndex((item) => item.productId === id);
-    removeCartItem(index);
+    togglePopup?.(
+      true,
+      <Popup
+        data={{ product: products[index] }}
+        onYes={() => {
+          removeCartItem(index);
+          togglePopup(false);
+        }}
+        onNo={() => togglePopup(false)}
+      />,
+    );
   };
 
   const handleCheckout = () => {
-    const redirectUrl = `/checkout`;
+    if (cartItems.length === 0) {
+      return;
+    }
+
+    const redirectUrl = '/checkout';
 
     if (!user) {
       navigate(
@@ -55,40 +108,62 @@ function CartPage() {
       window.location.href = redirectUrl;
     }
   };
+
   return (
     <Layout>
       <div className={styles['cart-page']}>
         <div className={styles['title']}>Your Cart:</div>
+
         <div className={styles['list-container']}>
           {products &&
             products.map((product, index) => (
               <div className={styles['product']} key={index}>
                 <div className={styles['image']}>
-                  <Image src={product.images[0]} />
+                  <Link to={`/products/${product.id}`}>
+                    <Image src={product.images[0]} />
+                  </Link>
                 </div>
+
                 <div className={styles['info']}>
-                  <div className={styles['name']}>{product.title}</div>
+                  <div className={styles['name']}>
+                    <Link to={`/products/${product.id}`}>{product.title}</Link>
+                  </div>
+
                   <div className={styles['quantity-container']}>
                     <label className={styles['label']}>Quantity:</label>
+
                     <div className={styles['quantity']}>
                       <NumberInput
                         className={styles['quantity-input']}
-                        value={getQuantity(product)}
+                        value={cartItems[index].quantity}
                         onChangeValue={(quantity: number) =>
-                          onChangeQuantity(product.id, quantity)
+                          handleChangeQuantity(index, quantity)
                         }
-                        min={1}
-                        max={product.quantity - product.soldQuantity}
+                        min={0}
+                        max={
+                          product.quantity -
+                          product.soldQuantity -
+                          cartItems[index].quantity
+                        }
                       />
                     </div>
                   </div>
+
                   <div className={styles['price-remove']}>
-                    <div className={styles['price']}>
-                      ${product.price * getQuantity(product)}
-                    </div>
+                    <Price
+                      className={styles['price']}
+                      data={{
+                        price:
+                          (product.type === ProductType.BuyNow
+                            ? product.price
+                            : product.price - (product.discount || 0)) *
+                          cartItems[index].quantity,
+                      }}
+                    />
+
                     <Button
                       className={styles['remove']}
-                      onClick={() => handleRemoveItem(product.id)}
+                      onClick={() => handleRemoveItem(index)}
                     >
                       Remove Item
                     </Button>
@@ -97,26 +172,41 @@ function CartPage() {
               </div>
             ))}
         </div>
+
         <div className={styles['footer']}>
           <div className={styles['subtotal']}>
-            Subtotal:{' '}
-            <span>
-              $
-              {products &&
-                products.reduce(
-                  (total, product) =>
-                    total + product.price * getQuantity(product),
-                  0,
-                )}
-            </span>
+            <div className={styles['label']}>Subtotal:</div>
+
+            <Price
+              className={styles['price']}
+              data={{
+                price: products
+                  ? products.reduce(
+                      (total, product, index) =>
+                        total +
+                        (product.type === ProductType.BuyNow
+                          ? product.price
+                          : product.price - (product.discount || 0)) *
+                          cartItems[index].quantity,
+                      0,
+                    )
+                  : 0,
+              }}
+            />
           </div>
-          <Button className={styles['checkout']} onClick={handleCheckout}>
+
+          <Button
+            className={styles['checkout']}
+            onClick={handleCheckout}
+            disabled={cartItems.length === 0}
+          >
             Proceed to Checkout
           </Button>
+
           <ButtonLink
             className={styles['chat']}
             theme="black"
-            to={`https://t.me/auctomarketplace`}
+            to="https://t.me/auctomarketplace"
             target="_blank"
           >
             Chat with us
